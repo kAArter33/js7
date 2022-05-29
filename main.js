108aac84b2110ca9fc072ee129b7eba624437e81
kbaginski <karter1998@o2.pl>
2022-05-29 17:54:04 +0200

    funkcja suma

---
+++ main.js
@@ -0,0 +1,18 @@
+let firstNumber = 10;
+let secondNumber = 20;
+
+
+function dodaj (firstNumber,secondNumber)
+
+{
+
+let result = firstNumber+secondNumber;
+
+return result;
+
+
+
+}
+
+dodaj(firstNumber,secondNumber);
+console.log(dodaj(firstNumber,secondNumber));
